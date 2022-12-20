9583ea0464607ccdf0e3da3dce7d75e60aea6e14
Samuel Carvalho <115167862+Rekyeme@users.noreply.github.com>
2022-12-20 13:51:08 +0000

    Display config and Quit event;

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
-import pygame, sys
+# Imports Pygame
+import pygame
 
+# Screen width and height;
 WIDTH, HEIGHT = 800, 600
 DISPALYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# Main loop;
 def main():
 
     run = True
@@ -14,5 +17,7 @@ def main():
 
     pygame.quit()
 
+
+# Guarantees that the program only runs the main function if this file is read directly;
 if __name__ == "__main__":
     main()
