65061879ed22b36ccb6707d373d1e8ab0fe16541
Samuel Carvalho <115167862+Rekyeme@users.noreply.github.com>
2022-12-20 13:42:10 +0000

    Display config

--- Main.py
+++ Main.py
@@ -1,1 +1,18 @@
 import pygame, sys
+
+WIDTH, HEIGHT = 800, 600
+DISPALYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type  == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
